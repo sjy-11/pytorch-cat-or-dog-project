53dee42fc37610ac5837660bc5a2f1d035f9ffa1
jy <147988964+sjy-11@users.noreply.github.com>
2024-03-22 20:55:56 +0800

    removed image dataset from repo

--- model/train_test.py
+++ model/train_test.py
@@ -1,10 +1,9 @@
 from model import Net
-
 import torch
 import torch.nn as nn
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 import torch.optim as optim
 import PIL as Image
 
@@ -12,3 +11,25 @@ import PIL as Image
 net = Net()
 
 #prepare the train and test datasets
+train_transform = transforms.Compose([
+    transforms.RandomRotation(45),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomAutocontrast(),
+    transforms.Resize((128, 128)),
+    transforms.ToTensor()
+])
+
+test_transforms = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor()
+])
+
+#image paths
+image_path_train = "../data/training"
+image_path_test = "../data/testing"
+
+
+
+
+
+
